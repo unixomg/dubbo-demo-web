0a8f0f6edc1a0af8d9a95dc577d6593d382298ba
shiyanbo <42290462+shiyanbo1991@users.noreply.github.com>
2020-01-04 16:43:21 +0800

    FFFF

--- dubbo-client/src/main/java/com/od/dubbotest/action/ShAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/ShAction.java
@@ -16,7 +16,7 @@ public class ShAction {
 	public String say(String name) {
 		System.out.println("HelloAction接收到请求:"+name);
 		String str="<h1>这是Dubbo 消费者端(tomcat)</h1>";
-		str+="<h2>这里是天地汇上海站的页面</h2>";
+		str+="<h2>这里是天地汇上海站的页面--灰度测试页面</h2>";
 		str+=helloService.hello(name);
 		System.out.println("HelloService返回到结果:"+str);
 		return str;
