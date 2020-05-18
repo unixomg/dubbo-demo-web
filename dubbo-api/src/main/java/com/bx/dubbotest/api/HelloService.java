bdf2858ed7798e843f44f51cebc3037ef448b082
liuxingzun <liuxingzun@360jinrong.net>
2020-05-18 21:55:23 +0800

    modify zk

---
+++ dubbo-api/src/main/java/com/bx/dubbotest/api/HelloService.java
@@ -0,0 +1,7 @@
+package com.bx.dubbotest.api;
+
+
+public interface HelloService {
+
+	public String hello(String name);
+}
---
+++ dubbo-client/src/main/java/com/bx/dubbotest/Application.java
@@ -0,0 +1,22 @@
+package com.bx.dubbotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.bx.dubbotest.action.HelloAction;
+import com.bx.dubbotest.action.BjAction;
+
+@SpringBootApplication
+public class Application {
+    public static void main( String[] args) {
+    	ApplicationContext context=SpringApplication.run("classpath*:spring-config.xml",args);
+    	HelloAction action=(HelloAction) context.getBean("helloAction");
+        BjAction bjAction=(BjAction) context.getBean("bjAction");
+        System.out.println("Dubbo client started");
+        System.out.println("Dubbo 消费者端启动");
+        //action.say("jack");
+        //action.say("tom");
+        //action.say("jim");
+    }
+}
